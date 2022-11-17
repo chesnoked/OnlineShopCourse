6c18292f7b8304adeae486e54e1c687e9f66f136
chesnoked <dreamspiritworld@gmail.com>
2022-11-18 00:48:34 +1000

    OnlineShopCourse. Step 34. Context menus. ViewBuilder.

--- OnlineShopCourse/Modules/Profile/ProfileView.swift
+++ OnlineShopCourse/Modules/Profile/ProfileView.swift
@@ -5,16 +5,18 @@
 //  Created by Evgeniy Safin on 15.10.2022.
 //
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
-}
+//struct ProfileView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProfileView()
+//    }
+//}
 
 import SwiftUI
 
 struct ProfileView: View {
     @EnvironmentObject private var profileVM: ProfileViewModel
+    @State private var showLeftContextMenu: Bool = false
+    @State private var showRightContextMenu: Bool = false
     @State private var selectedOrder: OrderModel? = nil
     @State private var myColorTheme: MyColorTheme = MyColorTheme()
     @State private var parentColor: Color = Color.palette.parent
@@ -22,8 +24,22 @@ struct ProfileView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                // color theme changer
-                colorThemeChanger
+                ZStack(alignment: showLeftContextMenu ? .topLeading : showRightContextMenu ? .topTrailing : .top) {
+                    // nav bar
+                    navBar
+                    // context menus
+                    Group {
+                        if showLeftContextMenu {
+                            leftContextMenu
+                        }
+                        if showRightContextMenu {
+                            rightContextMenu
+                        }
+                    }
+                    .transition(.opacity.animation(.linear(duration: 0.33)))
+                }
+                .padding([.horizontal, .vertical])
+                
                 // orders
                 orders
                     .padding([.horizontal, .vertical])
@@ -35,26 +51,140 @@ struct ProfileView: View {
 }
 
 extension ProfileView {
-    // color theme changer
-    private var colorThemeChanger: some View {
+    // nav bar
+    private var navBar: some View {
         HStack(spacing: 0) {
-            // parent color picker
-            ColorPicker(selection: $parentColor, supportsOpacity: true, label: { })
-                .onChange(of: parentColor) { newParentColor in
-                    myColorTheme.saveColor(color: newParentColor, forKey: "parent_color")
-                }
+            // left context menu
+            Button(action: {
+                showRightContextMenu = false
+                showLeftContextMenu.toggle()
+            }, label: {
+                Image(systemName: "gear")
+                    .glassomorphismTextFieldStyle()
+                    .opacity(showLeftContextMenu ? 0.0 : 1.0)
+            })
             Spacer()
-            Button(action: { myColorTheme.defaultColorTheme() }, label: { Text("set to default").foregroundColor(Color.palette.child) })
-            Spacer()
-            // child color picker
-            ColorPicker(selection: $childColor, supportsOpacity: true, label: { })
-                .onChange(of: childColor) { newChildColor in
-                    myColorTheme.saveColor(color: newChildColor, forKey: "child_color")
-                }
+            // right context menu
+            Button(action: {
+                showLeftContextMenu = false
+                showRightContextMenu.toggle()
+            }, label: {
+                Image(systemName: "person.fill")
+                    .glassomorphismTextFieldStyle()
+                    .opacity(showRightContextMenu ? 0.0 : 1.0)
+            })
         }
-        .labelsHidden()
-        .padding([.horizontal, .vertical])
     }
+}
+
+extension ProfileView {
+    // @ViewBuilder context menu one item
+    struct ContextMenuItem<Item:View>: View {
+        init(@ViewBuilder item: () -> Item) {
+            self.item = item()
+        }
+        let item: Item
+        var body: some View {
+            item
+        }
+    }
+    // @ViewBuilder custom context menu
+    struct CustomContexMenu<Content:View>: View {
+        init(@ViewBuilder content: () -> Content) {
+            self.content = content()
+        }
+        let content: Content
+        var body: some View {
+            VStack(spacing: 15) {
+                content
+            }
+            .padding([.horizontal, .vertical])
+            .frame(width: UIScreen.main.bounds.width * 0.33)
+            .glassomorphismTextFieldStyle()
+        }
+    }
+    // left context menu
+    private var leftContextMenu: some View {
+        CustomContexMenu {
+            // parent color picker
+            ContextMenuItem {
+                HStack(spacing: 0) {
+                    Text("parent color")
+                        .font(.caption)
+                        .foregroundColor(Color.palette.child)
+                    Spacer()
+                    parentColorPicker
+                }
+            }
+            // child color picker
+            ContextMenuItem {
+                HStack(spacing: 0) {
+                    Text("child color")
+                        .font(.caption)
+                        .foregroundColor(Color.palette.child)
+                    Spacer()
+                    childColorPicker
+                }
+            }
+            // default color theme
+            ContextMenuItem {
+                defaultColorTheme
+            }
+        }
+    }
+    // right context menu
+    private var rightContextMenu: some View {
+        CustomContexMenu {
+            ContextMenuItem {
+                HStack(spacing: 0) {
+                    Text("sign out")
+                        .font(.caption)
+                        .foregroundColor(Color.palette.child)
+                    Spacer()
+                    Button(action: {
+                        // sign out
+                    }, label: {
+                        Image(systemName: "door.left.hand.open")
+                            .font(.caption)
+                            .bold()
+                            .foregroundColor(Color.palette.child)
+                    })
+                }
+            }
+        }
+    }
+}
+
+extension ProfileView {
+    // parent color picker
+    private var parentColorPicker: some View {
+        ColorPicker(selection: $parentColor, supportsOpacity: true, label: { })
+            .labelsHidden()
+            .onChange(of: parentColor) { newParentColor in
+                myColorTheme.saveColor(color: newParentColor, forKey: "parent_color")
+            }
+    }
+    // child color picker
+    private var childColorPicker: some View {
+        ColorPicker(selection: $childColor, supportsOpacity: true, label: { })
+            .labelsHidden()
+            .onChange(of: childColor) { newChildColor in
+                myColorTheme.saveColor(color: newChildColor, forKey: "child_color")
+            }
+    }
+    // default color theme
+    private var defaultColorTheme: some View {
+        Button(action: {
+            myColorTheme.defaultColorTheme()
+        }, label: {
+            Text("default color theme")
+                .font(.caption)
+                .foregroundColor(Color.palette.child)
+        })
+    }
+}
+
+extension ProfileView {
     // orders
     private var orders: some View {
         ScrollView(.vertical, showsIndicators: true) {
